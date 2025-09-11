9ed3718edf6ead70f4674b6fc2684a31cc215a90
Madi Rei <madison.nicole.rei@gmail.com>
2025-09-11 16:06:37 -0700

    added letter pool list and draw_letters function

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,7 +1,22 @@
 from random import randint
 
+letter_pool = ['A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'B', 'B', 'C', 
+                'C', 'D', 'D', 'D', 'D', 'E', 'E', 'E', 'E', 'E', 'E', 'E',
+                'E', 'E', 'E', 'E', 'E', 'F', 'F', 'G', 'G', 'G', 'H', 'H',
+                'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'J', 'K', 'L',
+                'L', 'L', 'L', 'M', 'M', 'N', 'N', 'N', 'N', 'N', 'N', 'O',
+                'O', 'O', 'O', 'O', 'O', 'O', 'O', 'P', 'P', 'Q', 'R', 'R',
+                'R', 'R', 'R', 'R', 'S', 'S', 'S', 'S', 'T', 'T', 'T', 'T',
+                'T', 'T', 'U', 'U', 'U', 'U', 'V', 'V', 'W', 'W', 'X', 'Y',
+                'Y', 'Z']
+
 def draw_letters():
-    pass
+    letters = []
+
+    for letter in range(10):
+        letters.append(letter_pool[randint(0, 104)])
+    
+    return letters
 
 def uses_available_letters(word, letter_bank):
     pass
